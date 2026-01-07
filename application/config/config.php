66a783c93238c62d42a123c7f163914511447529
Anugrah0603 <akbarakbar0319@gmail.com>
2026-01-07 16:12:31 +0700

    update: change base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Sawit_Digital/';
+$config['base_url'] = 'http://localhost/sawit_digital/';
 
 /*
 |--------------------------------------------------------------------------
